4f6268cb08b0c166e49a2d024133d8bc4ebfd3d8
Tyler Kephart <tkephart96@gmail.com>
2023-06-07 12:30:47 -0500

    update wrangle.ipynb, add wrangle.py and explore

---
+++ wrangle.py
@@ -0,0 +1,57 @@
+'''
+Wrangle CA wildfire, tree, and air quality data from a pre-built dataset
+
+Functions:
+- get_fire2018
+- split_data
+- std
+'''
+
+##### IMPORTS #####
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+##### FUNCTIONS #####
+def get_fire2018():
+    '''Acquire pre-built 2018 data'''
+    return pd.read_csv('ca_fire2018.csv')
+
+def get_fire():
+    '''Acquire pre-built 1997-2018 data'''
+    return pd.read_csv('ca_fire.csv')
+
+def split_data(df):
+    '''Split into train, validate, test with a 60/20/20 ratio'''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=42)
+    train, validate = train_test_split(train_validate, test_size=.25, random_state=42)
+    return train, validate, test
+
+def std(train,validate,test,scale=None):
+    """
+    The function applies the Standard Scaler method to scale the numerical features of the train, validate,
+    and test datasets.
+    
+    :param train: a pandas DataFrame containing the training data
+    :param validate: The validation dataset, which is used to evaluate the performance of the model
+    during training and to tune hyperparameters
+    :param test: The "test" parameter is a dataset that is used to evaluate the performance of a machine
+    learning model that has been trained on the "train" dataset and validated on the "validate" dataset.
+    The "test" dataset is typically used to simulate real-world scenarios and to ensure that the model
+    is able
+    :return: three dataframes: Xtr (scaled training data), Xv (scaled validation data), and Xt (scaled
+    test data).
+    """
+    if scale is None:
+        scale = train.columns.to_list()
+    std_scale = StandardScaler()
+    Xtr,Xv,Xt = train[scale],validate[scale],test[scale]
+    Xtr = pd.DataFrame(std_scale.fit_transform(train[scale]),train.index,scale)
+    Xv = pd.DataFrame(std_scale.transform(validate[scale]),validate.index,scale)
+    Xt = pd.DataFrame(std_scale.transform(test[scale]),test.index,scale)
+    for col in scale:
+        Xtr = Xtr.rename(columns={col: f'{col}_s'})
+        Xv = Xv.rename(columns={col: f'{col}_s'})
+        Xt = Xt.rename(columns={col: f'{col}_s'})
+    return Xtr, Xv, Xt
