964ad1fc743a5606848017bc0b2d8a979d3ca269
Tyler Kephart <tkephart96@gmail.com>
2023-06-07 21:59:29 -0500

    add big wrangle

---
+++ big_wrangle.py
@@ -0,0 +1,361 @@
+'''
+Wrangle the data the same way I did
+
+BigQuery Stuff:
+- long_sql_air_quality
+- long_sql_usfs
+- small_sql_air_quality
+- small_sql_usfs
+- project_ID
+
+Functions:
+- wrangle_wildfires
+    - wrangle_forest_fires
+        - wrangle_fires
+            - get_zipped_fire
+            - get_fires
+            - prep_fires
+        - wrangle_forest
+            - get_forest
+            - prep_forest
+    - wrangle_air_quality
+        - get_air_quality
+        - prep_air_quality
+- split_data
+- std
+'''
+
+##### IMPORTS #####
+
+import pandas as pd
+import sqlite3
+import requests
+import zipfile
+import io
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+##### BIGQUERY STUFF #####
+
+# state_code 6 is CA
+# datum can't be unknown or NAD27 as I want lat long to be somewhat accurate
+# the 2.5ft error between NAD83 and WGS84 shouldn't affect this project (I think)
+# this in turn leaves me with data from 1997 but usfs only has from 2001
+long_sql_air_quality = """
+SELECT poc,latitude,longitude,parameter_name,sample_duration,
+date_local,units_of_measure,observation_count,
+arithmetic_mean,county_code,site_num
+FROM `bigquery-public-data.epa_historical_air_quality.co_daily_summary`
+WHERE state_code = '06' and date_local < '2019-01-01' and date_local > '2000-12-31' and datum != 'UNKNOWN' and datum != 'NAD27'
+UNION ALL
+SELECT poc,latitude,longitude,parameter_name,sample_duration,
+date_local,units_of_measure,observation_count,
+arithmetic_mean,county_code,site_num
+FROM `bigquery-public-data.epa_historical_air_quality.pressure_daily_summary`
+WHERE state_code = '06' and date_local < '2019-01-01' and date_local > '2000-12-31' and datum != 'UNKNOWN' and datum != 'NAD27'
+UNION ALL
+SELECT poc,latitude,longitude,parameter_name,sample_duration,
+date_local,units_of_measure,observation_count,
+arithmetic_mean,county_code,site_num
+FROM `bigquery-public-data.epa_historical_air_quality.rh_and_dp_daily_summary`
+WHERE state_code = '06' and date_local < '2019-01-01' and date_local > '2000-12-31' and datum != 'UNKNOWN' and datum != 'NAD27'
+UNION ALL
+SELECT poc,latitude,longitude,parameter_name,sample_duration,
+date_local,units_of_measure,observation_count,
+arithmetic_mean,county_code,site_num
+FROM `bigquery-public-data.epa_historical_air_quality.temperature_daily_summary`
+WHERE state_code = '06' and date_local < '2019-01-01' and date_local > '2000-12-31' and datum != 'UNKNOWN' and datum != 'NAD27'
+UNION ALL
+SELECT poc,latitude,longitude,parameter_name,sample_duration,
+date_local,units_of_measure,observation_count,
+arithmetic_mean,county_code,site_num
+FROM `bigquery-public-data.epa_historical_air_quality.wind_daily_summary`
+WHERE state_code = '06' and date_local < '2019-01-01' and date_local > '2000-12-31' and datum != 'UNKNOWN' and datum != 'NAD27'
+"""
+
+# usfs CA data is only from 2001 to 2018 (this is where I am limited most)
+long_sql_usfs = """
+SELECT  
+    measurement_year,
+    tree_county_code,latitude,longitude,elevation,
+    trees_per_acre_unadjusted,
+    water_on_plot_code_name,
+    species_common_name,species_group_code_name,
+    total_height,
+    current_diameter,
+    tree_status_code_name,
+    invasive_sampling_status_code_name,
+FROM `bigquery-public-data.usfs_fia.plot_tree`
+WHERE plot_state_code = 6 AND total_height > 0 AND measurement_year > 2000
+"""
+
+# EDIT THE BELOW 'FROM' lines in both small_sql variables
+# with your respective database and table names from your project
+# my database is `california_air_trees_fires`
+# my air quality table is `epa_aq_ca5`
+small_sql_air_quality = """
+SELECT *
+FROM `california_air_trees_fires.epa_aq_ca5`
+"""
+
+# my usfs table is `usfs_fia_pt_ca2`
+small_sql_usfs = """
+SELECT *
+FROM `california_air_trees_fires.usfs_fia_pt_ca2`
+"""
+
+# EDIT THE BELOW project_ID with your respective project ID
+project_ID = 'my-ds-projects'
+
+##### FUNCTIONS #####
+
+def get_zipped_fire():
+    '''Get the necessary sqlite file from the zip without having to save the whole zip'''
+    # url for zipped data
+    url = 'https://www.fs.usda.gov/rds/archive/products/RDS-2013-0009.6/RDS-2013-0009.6_SQLITE.zip'
+    # get zipped data
+    response = requests.get(url, stream=True)
+    z = zipfile.ZipFile(io.BytesIO(response.content))
+    # extract necessary file
+    z.extract('Data/FPA_FOD_20221014.sqlite')
+
+def get_fires():
+    '''Get fire data from sqlite file and download file if not there'''
+    # sqlite connection to file downloaded from zip
+    filename = 'Data/FPA_FOD_20221014.sqlite'
+    if not os.path.isfile(filename):
+        # caches sqlite file
+        get_zipped_fire()
+    # make connection to sqlite file
+    conn = sqlite3.connect(filename)
+    # read sql query
+    return pd.read_sql(
+                '''
+                        select 
+                        FIRE_YEAR,
+                        DISCOVERY_DATE,
+                        DISCOVERY_TIME,
+                        NWCG_CAUSE_CLASSIFICATION,
+                        NWCG_GENERAL_CAUSE,
+                        FIRE_SIZE,
+                        FIRE_SIZE_CLASS,
+                        LATITUDE,
+                        LONGITUDE,
+                        FIPS_CODE,
+                        FIPS_NAME
+                        from fires where STATE = :state
+                    ''',
+        conn,
+        params={'state': 'CA'},)
+
+def prep_fires(fire):
+    '''Prepare fire data'''
+    # drop geometric map shape
+    fire = fire.drop(columns='Shape')
+    # make the columns stop yelling at me
+    fire.columns = fire.columns.str.lower()
+    # get rid of a few nulls to create county_code column
+    fire = fire[fire['fips_code'].notna()].copy()
+    # need for merge
+    fire['county_code'] = fire.fips_code.str[2:].astype(int)
+    return fire
+
+def wrangle_fires():
+    '''Wrangle some fire'''
+    # get fire data
+    fire = get_fires()
+    # prep fire data and put it out
+    return prep_fires(fire)
+
+def get_air_quality():
+    '''Get the saved air quality data from personal project on BigQuery'''
+    # pandas gbq will auth with google account first, rerun again if necessary
+    return pd.read_gbq(small_sql_air_quality,dialect='standard',project_id=project_ID,use_bqstorage_api=True)
+
+def prep_air_quality(aq):
+    '''Prepare air quality data'''
+    # change to datetime, need for pivot and merge
+    aq.date_local = aq.date_local.astype('datetime64[ns]')
+    # create year column, need for concat
+    aq['year'] = aq.date_local.dt.year
+    # convert county_code to int, need for pivot and merge
+    aq.county_code = aq.county_code.astype(int)
+    return aq
+
+def wrangle_air_quality():
+    '''Wrangle some air'''
+    # get air quality
+    aq = get_air_quality()
+    # prep air data
+    aq = prep_air_quality(aq)
+    # set blank variable for fully pivoted data to concat on
+    caq = None
+    # loop thru years for easy pivot and concat
+    for year in range(2001,2019):
+        # air quality per year
+        sm_aq = aq[(aq.year==year)]
+        # pivot so row is each county on date with mean of each parameter
+        piv_aq = sm_aq.pivot_table(index=['date_local','county_code'],
+                                    columns='parameter_name',
+                                    values=['arithmetic_mean']).reset_index()
+        # concat together
+        caq = piv_aq if caq is None else pd.concat([caq,piv_aq],ignore_index=True)
+    # cough up air quality data
+    return caq
+
+def get_forest():
+    '''Get the saved forest data from personal project on BigQuery'''
+    # pandas gbq will auth with google account first, rerun again if necessary
+    return pd.read_gbq(small_sql_usfs,dialect='standard',project_id=project_ID,use_bqstorage_api=True)   
+
+def prep_forest(pt):
+    '''Prepare forest data'''
+    # I use pt because usfs table was called plot_tree
+    # drop them na trees
+    return pt[pt['tree_county_code'].notna()].copy()
+
+def wrangle_forest():
+    '''Wrangle some trees'''
+    # get tree data
+    pt = get_forest()
+    # prep trees and roll them out
+    return prep_forest(pt)
+
+def wrangle_forest_fires():
+    # get fire and forest data for merge
+    fire = wrangle_fires()
+    pt = wrangle_forest()
+    # set blank variable for fully merged data to concat on
+    fpt = None
+    # loop thru years for easy merge and concat
+    for year in range(2001,2019):
+        # fire each year
+        sm_fire = fire[fire.fire_year==year]
+        # tree each year
+        sm_pt = pt[pt.measurement_year==year]
+        # dropping tree nulls for merge
+        sm_pt = sm_pt.dropna()
+        # fire tree merge on county to trees
+        sm_fpt = pd.merge(left=sm_fire,right=sm_pt,how='right',
+                            left_on='county_code',right_on='tree_county_code')
+        # concat together
+        fpt = sm_fpt if fpt is None else pd.concat([fpt,sm_fpt],ignore_index=True)
+    # convert to datetime for merge with air quality
+    fpt.discovery_date = fpt.discovery_date.astype('datetime64[ns]')
+    # put out the forest fires
+    return fpt
+
+def wrangle_wildfires():
+    '''Wrangle together CA wildfire data'''
+    # get forest fire and air data for merge
+    fpt = wrangle_forest_fires()
+    caq = wrangle_air_quality()
+    # forest fire and air data merge on data and county to forest fire
+    ca = pd.merge(left=fpt,right=caq,how='left',
+                    left_on=['discovery_date','county_code'],
+                    right_on=[('date_local',''),('county_code','')])
+    # rename for readability and ease of use
+    ca.columns = [
+        'fire_year',
+        'date',
+        'time',
+        'cause_class',
+        'cause',
+        'fire_size',
+        'fire_size_class',
+        'lat',
+        'long',
+        'fips_code',
+        'county',
+        'county_code1',
+        'measurement_year',
+        'tree_county_code',
+        'latitude_y',
+        'longitude_y',
+        'elevation',
+        'trees_per_acre',
+        'water_on_plot',
+        'species',
+        'species_group',
+        'height',
+        'diameter',
+        'tree_alive',
+        'invasive_sampling',
+        'date_local',
+        'county_code2',
+        'bp_mean',
+        'co_mean',
+        'dp_mean',
+        'temp_mean',
+        'humidity_mean',
+        'wind_direction_mean',
+        'wind_speed_mean']
+    # keep necessary columns, drop others
+    ca = ca[[
+        'date',
+        'time',
+        'cause_class',
+        'cause',
+        'fire_size',
+        'fire_size_class',
+        'lat',
+        'long',
+        'elevation',
+        'county',
+        'trees_per_acre',
+        'water_on_plot',
+        'species',
+        'species_group',
+        'height',
+        'diameter',
+        'tree_alive',
+        'invasive_sampling',
+        'co_mean',
+        'temp_mean',
+        'humidity_mean',
+        'wind_direction_mean',
+        'wind_speed_mean']]
+    # get rid of nulls
+    ca = ca[(ca.co_mean.notna())&(ca.wind_speed_mean.notna())&(ca.humidity_mean.notna())&(ca.wind_direction_mean.notna())&(ca.time.notna())]
+    # map easy categorical values to 1 and 0
+    ca.tree_alive = ca.tree_alive.map({'Live tree':1,'Dead tree':0})
+    ca.invasive_sampling = ca.invasive_sampling.map({'Invasive plant data collected on all accessible land conditions':1,'Not collecting invasive plant data':0})
+    # cache locally
+    ca.to_csv('ca_fire.csv',index=False)
+    # put out wildfires
+    return ca
+
+def split_data(df):
+    '''Split into train, validate, test with a 60/20/20 ratio'''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=42)
+    train, validate = train_test_split(train_validate, test_size=.25, random_state=42)
+    return train, validate, test
+
+def std(train,validate,test,scale=None):
+    """
+    The function applies the Standard Scaler method to scale the numerical features of the train, validate,
+    and test datasets.
+    
+    :param train: a pandas DataFrame containing the training data
+    :param validate: The validation dataset, which is used to evaluate the performance of the model
+    during training and to tune hyperparameters
+    :param test: The "test" parameter is a dataset that is used to evaluate the performance of a machine
+    learning model that has been trained on the "train" dataset and validated on the "validate" dataset.
+    The "test" dataset is typically used to simulate real-world scenarios and to ensure that the model
+    is able
+    :return: three dataframes: Xtr (scaled training data), Xv (scaled validation data), and Xt (scaled
+    test data).
+    """
+    if scale is None:
+        scale = train.columns.to_list()
+    std_scale = StandardScaler()
+    Xtr,Xv,Xt = train[scale],validate[scale],test[scale]
+    Xtr = pd.DataFrame(std_scale.fit_transform(train[scale]),train.index,scale)
+    Xv = pd.DataFrame(std_scale.transform(validate[scale]),validate.index,scale)
+    Xt = pd.DataFrame(std_scale.transform(test[scale]),test.index,scale)
+    for col in scale:
+        Xtr = Xtr.rename(columns={col: f'{col}_s'})
+        Xv = Xv.rename(columns={col: f'{col}_s'})
+        Xt = Xt.rename(columns={col: f'{col}_s'})
+    return Xtr, Xv, Xt
